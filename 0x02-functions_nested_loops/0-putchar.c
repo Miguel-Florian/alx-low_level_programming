7921e9ae7d22336ff0f06ee0b476a528a9ad11d2
Miguel-Florian <migflor04@gmail.com>
2023-03-21 02:33:39 -0700

    Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int c,t;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int c, t;	
 
 	t = sizeof(str) / sizeof(int);
 	for (c = 0; c < t; c++)
