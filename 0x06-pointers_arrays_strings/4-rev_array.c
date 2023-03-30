f7bf96eb58cfa39dba67867fd580389691cc291e
Miguel-Florian <migflor04@gmail.com>
2023-03-30 13:20:54 -0700

    Write a function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse the contain of an array
+ *@*a: array
+ *@n: lentgth of array
+ *
+ * Return: no return
+ */
+void reverse_array(int *a,int n){
+   int i, tmp;
+
+	for (i = 0; i < n--; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
