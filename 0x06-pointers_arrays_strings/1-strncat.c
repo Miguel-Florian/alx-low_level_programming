6473efa451ae19c55e2044678937efe19da67813
Miguel-Florian <migflor04@gmail.com>
2023-03-30 13:19:34 -0700

    Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+* _strncpy - copies a string
+* @dest: pointer of string
+* @src: pointer of string
+* @n: int
+* Return: @dest pointer of string 
+*/
+
+int World_len(char *chaine){
+    int i;
+    for (i = 0; *chaine != '\0'; i++)
+        chaine++;
+    return i;
+}
+
+char *_strncat(char *dest, char *src, int n){
+    int dest_len = World_len(dest);
+    int j;
+
+    for (j = 0 ; j < n && src[j] != '\0' ; j++)
+        dest[dest_len + j] = src[j];
+    dest[dest_len + j] = '\0';
+
+    return dest;
+}
