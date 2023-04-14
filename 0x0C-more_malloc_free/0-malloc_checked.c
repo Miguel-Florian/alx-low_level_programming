7248d28bf623f215f4d6ea39fe2fbdd1f16bddfa
Miguel-Florian <migflor04@gmail.com>
2023-04-14 02:37:49 -0700

    Write a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,7 +11,7 @@ void *malloc_checked(unsigned int b)
 	
 	array = malloc(b);
 	if (array == NULL)
-		exit(0);
+		exit(98);
 	else
 		return (array);
 	/*printf((array == NULL) ? exit(0) : array);*/
