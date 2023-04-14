2c4e04394b5dd51f058e8f00842f06e9654dc6c7
Miguel-Florian <migflor04@gmail.com>
2023-04-14 06:00:32 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,33 +10,32 @@
  *
  * Return: pointer to the allocated memory.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int i)
 {
-        unsigned int i;
-        char *res;
+	char *res;
+	unsigned int i;
 
-        if (s1 == NULL)
-                s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-        if (s2 == NULL)
-                s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        if (n > strlen(s2))
-                n = strlen(s2);
+	if (n > strlen(s2))
+		n = strlen(s2);
 
-        res = malloc((strlen(s1) + n) + 1);
+	res = malloc((strlen(s1) + n) + 1);
 
-        if (res == NULL)
-                return (NULL);
+	if (res == NULL)
+		return (NULL);
 
-        for (i = 0; i < strlen(s1) + n; i++)
-        {
-                if (i < strlen(s1))
-                        res[i] = s1[i];
-                else
-                        res[i] = s2[i - strlen(s1)];
-        }
+	for (i = 0; i < (streln(s1) + n); i++)
+		if (i < strlen(s1))
+			res[i] = s1[i];
+		else
+			res[i] = s2[i - strlen(s1)];
+
 	res[i] = '\0';
 
-        return (res);
+	return (res);
 }
