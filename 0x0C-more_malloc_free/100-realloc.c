1c82ad7500d9ab980c2b6f7dbbd23e6683023cd6
Miguel-Florian <migflor04@gmail.com>
2023-04-14 07:29:44 -0700

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,14 +13,14 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
 
 	if (new_size == old_size)
 		return (ptr);
