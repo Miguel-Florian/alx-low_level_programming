09e3f58c1275a6515c27d85ddccb105e55ca1a46
Miguel-Florian <migflor04@gmail.com>
2023-04-17 13:03:58 -0700

    dog header file, define a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifdef _DOG_H_
+#define _DOG_H
+/**
+ * dog - define a struct dog
+ */
+typedef struct dog dog;
+/**
+ * dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
