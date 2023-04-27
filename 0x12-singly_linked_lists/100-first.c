57654c5610c6fd1dd8546f4e334a349c0093fe6d
Miguel-Florian <migflor04@gmail.com>
2023-04-27 16:30:10 -0700

    Write a function that prints You're beat! and yet, you must allow,I bore my house upon my back! before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * bmain - function executed before main
  * Return: no return.
  */
-
 void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow \nI bore my house upon my back!\n");
+	printf ("You're beat! and yet, you must allow \nI bore my house upon my back!\n");
 }
