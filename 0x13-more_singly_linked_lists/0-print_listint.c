ec15bea698734c548b260cc7a8c4ca1b65bc4951
Miguel-Florian <migflor04@gmail.com>
2023-05-01 11:13:28 -0700

    0x13 C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,7 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a list.
  * @h: head of a list.
- *
  * Return: numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
